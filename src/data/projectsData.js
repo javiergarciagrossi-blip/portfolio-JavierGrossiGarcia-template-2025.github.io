4fea5d7e95a495ba1cf97d0c7e9d30184f1a3c35
Javier Grossi Garcia <javiergarciagrossi@gmail.com>
2026-01-08 18:42:23 +0100

    Fix imagenes

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -6,7 +6,7 @@ export const projectsData = [
     description: "Campaña publicitaria de Chupa Chups basada en 5 carteles, correlacionado entre sí.",
     fullDescription: "La campaña se basa en la premisa de que la vida adulta es a menudo gris, aburrida y repetitiva, pero introducir un Chupa Chups en la ecuación transforma instantáneamente la realidad en algo surrealista, divertido y vibrante.",
     tags: ["React", "Tailwind", "Stripe"],
-    image: "./02.jpg",
+    image: "public/02.jpg",
     link: "#", // Link a la demo
     github: "#" // Link al repo
   },
@@ -16,7 +16,7 @@ export const projectsData = [
     description: "Tres carteles para concursos.",
     fullDescription: "Una exploración gráfica sobre cómo la tipografía, el color y la composición pueden definir la identidad de un evento competitivo. Esta serie de tres carteles aborda temáticas opuestas (Cine, Hackathon y Arte Urbano) unificadas por un estilo visual audaz y contemporáneo.",
     tags: ["Vue", "Chart.js", "CSS Grid"],
-    image: "./animayo.png",
+    image: "public/animayo.png",
     link: "#",
     github: "#"
   },
@@ -27,7 +27,7 @@ export const projectsData = [
     description: "Bowlz, campaña de ramen instantáneo.",
     fullDescription: "La campaña desafía la etiqueta tradicional de la mesa. Bowlz no es una comida triste de oficina; es una declaración de intenciones. Es rápido,",
     tags: ["JS Vanilla", "API REST", "Glassmorphism"],
-    image: "./bowlz.png",
+    image: "public/bowlz.png",
     link: "#",
     github: "#"
   },
@@ -37,7 +37,7 @@ export const projectsData = [
     description: "Cómetelo todo, que hay personas que pasan hambre",
     fullDescription: "El título nace de esa frase que todas las madres han dicho alguna vez. El objetivo del proyecto es resignificar esa frase: ya no es una regañina para que te termines el plato por obligación, sino una herramienta para compartir lo que sobra por solidaridad y sostenibilidad.",
     tags: ["Socket.io", "Node.js", "React", "MongoDB"],
-    image: "./Casestudy Rodilla.png",
+    image: "public/Casestudy Rodilla.png",
     link: "#",
     github: "#"
   },
@@ -47,7 +47,7 @@ export const projectsData = [
     description: "Uso de la fotografía dentro del ámbito de la Dirección de Arte basándose en el mundo de Twin Peaks.",
     fullDescription: "Este proyecto no es un simple tributo, es una deconstrucción de la dirección de arte de David Lynch aplicada a una editorial de moda/producto moderna. Se explora la dualidad: la fachada de normalidad americana (el Sueño Americano) frente a la oscuridad surrealista que habita debajo.",
     tags: ["OpenAI API", "React", "Cloudinary"],
-    image: "./MockUp_revistaAudrey.jpg",
+    image: "public/MockUp_revistaAudrey.jpg",
     link: "#",
     github: "#"
   },
@@ -57,7 +57,7 @@ export const projectsData = [
     description: "Colepción de tres libros troquelados.",
     fullDescription: "Hollownight no es una lectura pasiva; es una exploración física. La colección utiliza la técnica del troquelado (cortes en el papel) para crear un efecto de libro túnel. A medida que pasas las páginas,",
     tags: ["PWA", "Firebase", "PWA", "ChartJS"],
-    image: "./mockuphollownight.png",
+    image: "public/mockuphollownight.png",
     link: "#",
     github: "#"
   }
