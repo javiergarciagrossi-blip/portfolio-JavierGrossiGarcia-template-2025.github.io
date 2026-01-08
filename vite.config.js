690b26584d93b9de43a387eb85ac54120ff0178d
Javier Grossi Garcia <javiergarciagrossi@gmail.com>
2026-01-08 18:00:29 +0100

    Fix vite config and build

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Asegúrate de que el nombre del repositorio sea el correcto aquí
+  base: '/portfolio-JavierGrossiGarcia-template-2025.github.io/',
+})
